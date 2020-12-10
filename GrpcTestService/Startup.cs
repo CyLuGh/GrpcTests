f80b7b24d90df1cc8f79e8ec8568864aa315cb2b
Jeremy Demortier <jeremy.demortier@nbb.be>
2020-12-10 16:36:58 +0100

    Force 5002 as http port

--- GrpcTestService/Startup.cs
+++ GrpcTestService/Startup.cs
@@ -27,14 +27,14 @@ namespace GrpcTestService
             app.UseRouting();
 
             app.UseEndpoints( endpoints =>
-             {
-                 endpoints.MapGrpcService<MicroPocService>();
+            {
+                endpoints.MapGrpcService<MicroPocService>();
 
-                 endpoints.MapGet( "/" , async context =>
-                  {
-                     await context.Response.WriteAsync( "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909" );
-                 } );
-             } );
+                endpoints.MapGet( "/" , async context =>
+                {
+                    await context.Response.WriteAsync( "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909" );
+                } );
+            } );
         }
     }
 }
--- TestPython/poc_client.py
+++ TestPython/poc_client.py
@@ -8,7 +8,7 @@ import poc_pb2
 import poc_pb2_grpc
 
 def run():
-    with grpc.insecure_channel('localhost:5000') as channel:
+    with grpc.insecure_channel('localhost:5002') as channel:
         stub = poc_pb2_grpc.MicroPocStub(channel)
         response = stub.Sum(poc_pb2.SumRequest(first=3,second=4))
     print(response.result)
