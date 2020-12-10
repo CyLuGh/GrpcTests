6f48260e157edc16212af88ebffe1c0ad47ba1f5
Jeremy Demortier <jeremy.demortier@nbb.be>
2020-12-10 09:32:13 +0100

    First test: C# only

---
+++ GrpcConsoleClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Grpc.Net.Client;
+using GrpcShared;
+using Spectre.Console;
+
+namespace GrpcConsoleClient
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            AnsiConsole.MarkupLine( "Let's sum [red]3[/] and [red]4[/]!" );
+
+            var channel = GrpcChannel.ForAddress( "https://localhost:5001" );
+            var client = new MicroPoc.MicroPocClient( channel );
+
+            var result = client.Sum( new SumRequest { First = 3 , Second = 4 } );
+
+            AnsiConsole.MarkupLine( "Result is [red]{0}[/]" , result.Result );
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcTestService/Services/MicroPocService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcShared;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcTestService.Services
+{
+    public class MicroPocService : MicroPoc.MicroPocBase
+    {
+        //private readonly ILogger<MicroPocService> _logger;
+        //public MicroPocService( ILogger<MicroPocService> logger )
+        //{
+        //    _logger = logger;
+        //}
+
+        public override Task<SumReply> Sum( SumRequest request , ServerCallContext context )
+            => Task.FromResult( new SumReply() { Result = request.First + request.Second } );
+    }
+}
